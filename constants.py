48553767e236a225dd9852825543f13a6cf2ff1a
PF <pierre-francois.gimenez@laposte.net>
2019-03-02 10:33:21 +0100

    Update entity with constants

--- constants.py
+++ constants.py
@@ -18,6 +18,9 @@ base1 = tcod.Color(147,161,161)
 base2 = tcod.Color(238,232,213)
 base3 = tcod.Color(253,246,227)
 
+door_color = tcod.Color(203,75,22)
+tcod.color_scale_HSV(door_color, 0.7, 1)
+
 class FeatureSlot(enum.Enum):
     m = {"name": "monsters", "color": magenta}
     i = {"name": "id", "color": violet}
@@ -31,27 +34,27 @@ class WeaponSlot(enum.Enum):
     hack = {"name": "hack", "success_rate_base": 1, "duration_base": 4, "instable": True}
 
 class FeatureEgo(enum.Enum):
-    c1 = {"name": "evolutive"}
-    c2 = {"name": "diplomatic"}
-    c3 = {"eame": "artistic"}
+    c1 = {"name": "evolutive", "char": "C"}
+    c2 = {"name": "diplomatic", "char": "C"}
+    c3 = {"eame": "artistic", "char": "C"}
 
-    b1 = {"name": "minigame-based"}
-    b2 = {"name": "reputation-based"}
-    b3 = {"name": "pun-based"}
+    b1 = {"name": "minigame-based", "char": "B"}
+    b2 = {"name": "reputation-based", "char": "B"}
+    b3 = {"name": "pun-based", "char": "B"}
 
-    p1 = {"name": "recursive"}
-    p2 = {"name": "meta"}
-    p3 = {"name": "self-contained"}
+    p1 = {"name": "recursive", "char": "P"}
+    p2 = {"name": "meta", "char": "P"}
+    p3 = {"name": "self-contained", "char": "P"}
 
-    m1 = {"name": "Sisyphean"}
-    m2 = {"name": "Lovecraftian"}
-    m3 = {"name": "Enochian"}
+    m1 = {"name": "Sisyphean", "char": "M"}
+    m2 = {"name": "Lovecraftian", "char": "M"}
+    m3 = {"name": "Enochian", "char": "M"}
 
 class WeaponEgo(enum.Enum):
-    c = {"name": "conscious", "fego": [FeatureEgo.c1, FeatureEgo.c2, FeatureEgo.c3]}
-    b = {"name": "basic", "fego": [FeatureEgo.b1, FeatureEgo.b2, FeatureEgo.b3]}
-    p = {"name": "paradoxical", "fego": [FeatureEgo.p1, FeatureEgo.p2, FeatureEgo.p3]}
-    m = {"name": "mythical", "fego": [FeatureEgo.m1, FeatureEgo.m2, FeatureEgo.m3]}
+    c = {"name": "conscious", "fego": [FeatureEgo.c1, FeatureEgo.c2, FeatureEgo.c3], "char": "c"}
+    b = {"name": "basic", "fego": [FeatureEgo.b1, FeatureEgo.b2, FeatureEgo.b3], "char": "b"}
+    p = {"name": "paradoxical", "fego": [FeatureEgo.p1, FeatureEgo.p2, FeatureEgo.p3], "char": "p"}
+    m = {"name": "mythical", "fego": [FeatureEgo.m1, FeatureEgo.m2, FeatureEgo.m3], "char": "m"}
 
 class TurnType(enum.Enum):
     ENNEMY = 0
@@ -59,5 +62,6 @@ class TurnType(enum.Enum):
     SPAWN = 2
 
 class FloorType(enum.Enum):
-    WALL = {"collision": True}
-    FLOOR = {"collision": False}
+    WALL = {"name": "wall", "collision": True, "transparent": False, "char": "#"}
+    FLOOR = {"name": "floor", "collision": False, "transparent": True, "char": "."}
+    DOOR = {"name": "door", "collision": False, "transparent": False, "char": "+"}
--- entity.py
+++ entity.py
@@ -1,17 +1,19 @@
 import tcod
+import constants as const
 
 class Entity:
     """
     A generic entity
     """
 
-    def __init__(self, x, y, char, color, name, collision, is_seen=False):
+    def __init__(self, x, y, char, color, name, collision, transparent, is_seen=False):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.collision = collision
+        self.transparent = transparent
         self.is_seen = is_seen
 
 class Floor(Entity):
@@ -20,7 +22,7 @@ class Floor(Entity):
     """
 
     def __init__(self, x, y, ftype, is_seen=False):
-        super().__init__(x, y, ftype.char, None, ftype.name, ftype.collision, is_seen) # TODO color
+        super().__init__(x, y, ftype.value.get("char"), const.base2, ftype.value.get("name"), ftype.value.get("collision"), ftype.value.get("transparent"), is_seen) # TODO color
         self.ftype = ftype
 
 class Weapon(Entity):
@@ -29,9 +31,10 @@ class Weapon(Entity):
     """
 
     def __init__(self, wslot, success_rate, duration, wego, level):
-        super().__init__(None, None, wego.char, wego.fslot.color, wego.name, collision=False)
+        super().__init__(None, None, wego.value.get("char"), const.base2, wego.value.get("name")+" "+wslot.value.get("name"), collision=False, transparent=True)
         self.wego = wego
         self.level = level
+        # TODO : use wslot
         self.success_rate = success_rate
         self.duration = duration
         self.wslot = wslot
@@ -39,7 +42,7 @@ class Weapon(Entity):
 class Feature(Entity):
 
     def __init__(self, fslot, fego, level, stability):
-        super().__init__(None, None, fego.char, fslot.color, fego.name, collision=False)
+        super().__init__(None, None, fego.value.get("char"), fslot.value.get("color"), fego.value.get("name")+" "+fslot.value.get("name"), collision=False, transparent=True)
         self.flot = fslot
         self.fego = fego
         self.level = level
@@ -47,11 +50,11 @@ class Feature(Entity):
 
 class Player(Entity):
     def __init__(self, x, y):
-        super().__init__(x, y, '@', None, "You", collision=True, is_seen=True) # TODO color
+        super().__init__(x, y, '@', None, "You", collision=True, transparent=True, is_seen=True) # TODO color
 
 class Monster(Entity):
     def __init__(self, x, y, fslot, hp, speed, fcreator):
-        super().__init__(x, y, str(hp), fslot.color, fslot+" bug", collision=True)
+        super().__init__(x, y, str(hp), fslot.value.get("color"), fslot+" bug", collision=True, transparent=True)
         self.fslot = fslot
         self.hp = hp
         self.speed = speed
